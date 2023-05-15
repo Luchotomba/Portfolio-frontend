3b0ac0bc3f63a71e57531d8d367a7f3d4ab94df6
giumolaro159 <123484973+giumolaro159@users.noreply.github.com>
2023-05-15 20:58:36 -0300

    commit frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstudiosComponent } from './componentes/Estudios/estudios/estudios.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias/experiencias.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades/habilidades.component';
+import { HeaderComponent } from './componentes/header/header/header.component';
+import { InicioComponent } from './componentes/Inicio/inicio/inicio.component';
+import { SobreMiComponent } from './componentes/Sobre Mi/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudiosComponent,
+    ExperienciasComponent,
+    HabilidadesComponent,
+    HeaderComponent,
+    InicioComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
